97d9074d4eba822babaca574193fc79f323768e5
Kishan <kishanshukla53@gmail.com>
2025-02-18 22:29:57 +0530

    init 3

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,9 +1,23 @@
-import React from "react";
-
-function App() {
-  return <div style={{fontSize:"300px", color:"crimson", }}>
-   kishan
-  </div>;
+export default function App() {
+  return <>
+  <h1 style={{color:"Window",backgroundColor:"black"}}>Sample Picture</h1>
+  <img src="https://cdn.britannica.com/94/494-050-A674AD3A/Fallow-deer-dama-dama.jpg?w=300" height={150}  width={150} style={{margin:"3px"}}/>
+  <img src="https://cdn.britannica.com/94/494-050-A674AD3A/Fallow-deer-dama-dama.jpg?w=300" height={150}  width={150} style={{margin:"3px"}}/>
+  <img src="https://cdn.britannica.com/94/494-050-A674AD3A/Fallow-deer-dama-dama.jpg?w=300" height={150}  width={150} style={{margin:"3px"}}/>
+  <img src="https://cdn.britannica.com/94/494-050-A674AD3A/Fallow-deer-dama-dama.jpg?w=300" height={150}  width={150} style={{margin:"3px"}}/>
+  <img src="https://cdn.britannica.com/94/494-050-A674AD3A/Fallow-deer-dama-dama.jpg?w=300" height={150}  width={150} style={{margin:"3px"}}/>
+  <img src="https://cdn.britannica.com/94/494-050-A674AD3A/Fallow-deer-dama-dama.jpg?w=300" height={150}  width={150} style={{margin:"3px"}}/>
+  <img src="https://cdn.britannica.com/94/494-050-A674AD3A/Fallow-deer-dama-dama.jpg?w=300" height={150}  width={150} style={{margin:"3px"}}/>
+  <img src="https://cdn.britannica.com/94/494-050-A674AD3A/Fallow-deer-dama-dama.jpg?w=300" height={150}  width={150} style={{margin:"3px"}}/>
+  <img src="https://cdn.britannica.com/94/494-050-A674AD3A/Fallow-deer-dama-dama.jpg?w=300" height={150}  width={150} style={{margin:"3px"}}/>
+  <img src="https://cdn.britannica.com/94/494-050-A674AD3A/Fallow-deer-dama-dama.jpg?w=300" height={150}  width={150} style={{margin:"3px"}}/>
+  <img src="https://cdn.britannica.com/94/494-050-A674AD3A/Fallow-deer-dama-dama.jpg?w=300" height={150}  width={150} style={{margin:"3px"}}/>
+  <img src="https://cdn.britannica.com/94/494-050-A674AD3A/Fallow-deer-dama-dama.jpg?w=300" height={150}  width={150} style={{margin:"3px"}}/>
+  <img src="https://cdn.britannica.com/94/494-050-A674AD3A/Fallow-deer-dama-dama.jpg?w=300" height={150}  width={150} style={{margin:"3px"}}/>
+  <img src="https://cdn.britannica.com/94/494-050-A674AD3A/Fallow-deer-dama-dama.jpg?w=300" height={150}  width={150} style={{margin:"3px"}}/>
+  <img src="https://cdn.britannica.com/94/494-050-A674AD3A/Fallow-deer-dama-dama.jpg?w=300" height={150}  width={150} style={{margin:"3px"}}/>
+  <img src="https://cdn.britannica.com/94/494-050-A674AD3A/Fallow-deer-dama-dama.jpg?w=300" height={150}  width={150} style={{margin:"3px"}}/>
+  <img src="https://cdn.britannica.com/94/494-050-A674AD3A/Fallow-deer-dama-dama.jpg?w=300" height={150}  width={150} style={{margin:"3px"}}/>
+  <img src="https://cdn.britannica.com/94/494-050-A674AD3A/Fallow-deer-dama-dama.jpg?w=300" height={150}  width={150} style={{margin:"3px"}}/>
+  </>
 }
-
-export default App;
