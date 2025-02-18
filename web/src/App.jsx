fc54f4f5265b0fcfa474aa5652dd8dd129c630ab
Kishan <kishanshukla53@gmail.com>
2025-02-18 23:24:29 +0530

    usestateHook

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,23 +1,27 @@
-export default function App() {
-  return <>
-  <h1 style={{color:"Window",backgroundColor:"black"}}>Sample Picture</h1>
-  <img src="https://cdn.britannica.com/94/494-050-A674AD3A/Fallow-deer-dama-dama.jpg?w=300" height={150}  width={150} style={{margin:"3px"}}/>
-  <img src="https://cdn.britannica.com/94/494-050-A674AD3A/Fallow-deer-dama-dama.jpg?w=300" height={150}  width={150} style={{margin:"3px"}}/>
-  <img src="https://cdn.britannica.com/94/494-050-A674AD3A/Fallow-deer-dama-dama.jpg?w=300" height={150}  width={150} style={{margin:"3px"}}/>
-  <img src="https://cdn.britannica.com/94/494-050-A674AD3A/Fallow-deer-dama-dama.jpg?w=300" height={150}  width={150} style={{margin:"3px"}}/>
-  <img src="https://cdn.britannica.com/94/494-050-A674AD3A/Fallow-deer-dama-dama.jpg?w=300" height={150}  width={150} style={{margin:"3px"}}/>
-  <img src="https://cdn.britannica.com/94/494-050-A674AD3A/Fallow-deer-dama-dama.jpg?w=300" height={150}  width={150} style={{margin:"3px"}}/>
-  <img src="https://cdn.britannica.com/94/494-050-A674AD3A/Fallow-deer-dama-dama.jpg?w=300" height={150}  width={150} style={{margin:"3px"}}/>
-  <img src="https://cdn.britannica.com/94/494-050-A674AD3A/Fallow-deer-dama-dama.jpg?w=300" height={150}  width={150} style={{margin:"3px"}}/>
-  <img src="https://cdn.britannica.com/94/494-050-A674AD3A/Fallow-deer-dama-dama.jpg?w=300" height={150}  width={150} style={{margin:"3px"}}/>
-  <img src="https://cdn.britannica.com/94/494-050-A674AD3A/Fallow-deer-dama-dama.jpg?w=300" height={150}  width={150} style={{margin:"3px"}}/>
-  <img src="https://cdn.britannica.com/94/494-050-A674AD3A/Fallow-deer-dama-dama.jpg?w=300" height={150}  width={150} style={{margin:"3px"}}/>
-  <img src="https://cdn.britannica.com/94/494-050-A674AD3A/Fallow-deer-dama-dama.jpg?w=300" height={150}  width={150} style={{margin:"3px"}}/>
-  <img src="https://cdn.britannica.com/94/494-050-A674AD3A/Fallow-deer-dama-dama.jpg?w=300" height={150}  width={150} style={{margin:"3px"}}/>
-  <img src="https://cdn.britannica.com/94/494-050-A674AD3A/Fallow-deer-dama-dama.jpg?w=300" height={150}  width={150} style={{margin:"3px"}}/>
-  <img src="https://cdn.britannica.com/94/494-050-A674AD3A/Fallow-deer-dama-dama.jpg?w=300" height={150}  width={150} style={{margin:"3px"}}/>
-  <img src="https://cdn.britannica.com/94/494-050-A674AD3A/Fallow-deer-dama-dama.jpg?w=300" height={150}  width={150} style={{margin:"3px"}}/>
-  <img src="https://cdn.britannica.com/94/494-050-A674AD3A/Fallow-deer-dama-dama.jpg?w=300" height={150}  width={150} style={{margin:"3px"}}/>
-  <img src="https://cdn.britannica.com/94/494-050-A674AD3A/Fallow-deer-dama-dama.jpg?w=300" height={150}  width={150} style={{margin:"3px"}}/>
-  </>
+import React from 'react'
+import { useState } from 'react'
+
+function App() {
+  const[naam,changeKaroNaam] = useState({
+    name:"kishan",
+    class :"4th year"
+  })
+  const raushan = {
+    name:"raushan",
+    class:"3rd year"
+  }
+
+  console.log("initial value "+naam.name + " " + naam.class)
+  return (
+    <div>
+      <h1>{naam.name}</h1>
+      <h1>{naam.class}</h1>
+    <button onClick={()=>changeKaroNaam(raushan)}>raushan</button>
+    {/* <button onClick={()=>changeKaroNaam("Subodh")}>subodh</button>
+    <button onClick={()=>setname("prince")}>prince</button>
+    <button onClick={()=>setname("Cutie")}>cutie</button> */}
+    </div>
+  )
 }
+
+export default App
